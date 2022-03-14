70f0b4cc61296ac048e00ae7898a7c68d52c05da
katrina2209 <katrina22@mail.ru>
2022-03-14 21:31:33 +0300

    logic changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val likes = 21
-    val people = if (likes == 1 || likes % 10 == 1) "человеку" else "людям"
+    val likes = 1511
+    val people = if (likes == 1 || (likes % 100 != 11 && likes % 10 == 1)) "человеку" else "людям"
 
     println("Понравилось $likes $people")
 }
